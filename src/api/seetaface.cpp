7e26a8e4ebbd517bad30408ac422d9ccab868d52
twmht <qrnnis2623891@gmail.com>
2019-07-20 18:02:01 +0800

    add pointdetector

--- src/api/seetaface.cpp
+++ src/api/seetaface.cpp
@@ -1,11 +1,13 @@
 #include <pybind11/pybind11.h>
 #include <seeta/FaceDetector2.h>
+#include <seeta/PointDetector2.h>
 #include <seeta/Struct_cv.h>
 // #include <opencv2/core/core.hpp>
 #include <opencv2/opencv.hpp>
 #include <pybind11/stl_bind.h>
 
 PYBIND11_MAKE_OPAQUE(std::vector<SeetaRect>);
+PYBIND11_MAKE_OPAQUE(std::vector<SeetaPointF>);
 
 namespace py = pybind11;
 
@@ -42,27 +44,24 @@ class FaceDetector {
     std::unique_ptr<seeta::FaceDetector2> detector;
 };
 
-// class PointDetector {
-  // public:
-    // PointDetector(const std::string& model) {
-      // detector.reset(new seeta::FaceDetector2(model.c_str()));
-    // }
+class PointDetector {
+  public:
+    PointDetector(const std::string& model) {
+      detector.reset(new seeta::PointDetector2(model.c_str()));
+    }
 
-    // std::vector<SeetaRect> detect(const std::string &path) {
-      // cv::Mat mat = cv::imread(path.c_str());
-      // seeta::cv::ImageData image = mat;
-      // int num;
-      // SeetaRect *rects = detector->Detect(image, &num);
-      // std::vector<SeetaRect> result;
-      // for (int i = 0; i < num; i++) {
-        // SeetaRect rect = rects[i];
-        // result.push_back(rect);
-      // }
-      // return result;
-    // }
-  // private:
-    // std::unique_ptr<seeta::PointDetector2> detector;
-// };
+    std::vector<SeetaPointF> detect(std::shared_ptr<SeetaImage> image, SeetaRect rect) {
+      SeetaPointF *points = detector->Detect(image->data(), rect);
+      std::vector<SeetaPointF> result;
+      for (int i = 0; i < detector->LandmarkNum(); i++) {
+        SeetaPointF point = points[i];
+        result.push_back(point);
+      }
+      return result;
+    }
+  private:
+    std::unique_ptr<seeta::PointDetector2> detector;
+};
 
 PYBIND11_MODULE(seetaface, m) {
 
@@ -73,12 +72,22 @@ PYBIND11_MODULE(seetaface, m) {
     .def_readwrite("width", &SeetaRect::width)
     .def_readwrite("height", &SeetaRect::height);
 
+  py::class_<SeetaPointF, std::unique_ptr<SeetaPointF>>(m, "SeetaPoint")
+    .def(py::init<>())
+    .def_readwrite("x", &SeetaPointF::x)
+    .def_readwrite("y", &SeetaPointF::y);
+
   py::class_<SeetaImage, std::shared_ptr<SeetaImage>>(m, "SeetaImage")
     .def(py::init<const std::string &>());
 
   py::bind_vector<std::vector<SeetaRect>>(m, "VectorSeetaRect");
+  py::bind_vector<std::vector<SeetaPointF>>(m, "VectorSeetaPoint");
 
   py::class_<FaceDetector, std::unique_ptr<FaceDetector>>(m, "FaceDetector")
     .def(py::init<const std::string&>())
     .def("detect", &FaceDetector::detect);
+
+  py::class_<PointDetector, std::unique_ptr<PointDetector>>(m, "PointDetector")
+    .def(py::init<const std::string&>())
+    .def("detect", &PointDetector::detect);
 }
